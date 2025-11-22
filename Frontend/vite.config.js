b6eec71e09125729847e3bd639d4e0f41c5a5674
Dev4057 <devanggandhi04@gmail.com>
2025-11-22 16:39:25 +0000

    Fix : package.json and making vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Vault_Guard/',  // Important: Must match your repo name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
+EOF
